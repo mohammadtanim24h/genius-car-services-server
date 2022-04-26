f38dfdef7c258ec096e271718880b9abc26f8ada
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-26 15:37:46 +0600

    added jwt auth

--- index.js
+++ index.js
@@ -11,7 +11,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// bla bla bla
+// Verify jwt
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+        return res.status(401).send({message: 'Unauthorized access'})
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err) {
+            return res.status(403).send({message: 'Forbidden Access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.evij0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -25,6 +39,7 @@ async function run() {
         // AUTH JWT
         app.post("/login", async (req, res) => {
             const user = req.body;
+            console.log(user);
             const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
             res.send({accessToken});
         })
@@ -67,12 +82,19 @@ async function run() {
 
 
         // Order collection api
-        app.get("/order", async (req, res) => {
-            const query = req.query;
-            console.log(query);
-            const cursor = orderCollection.find(query);
-            const orders = await cursor.toArray();
-            res.send(orders);
+        app.get("/order", verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if(email === decodedEmail){
+                const query = req.query;
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else {
+                res.status(403).send({message: 'Forbidden Access'})
+            }
+            
         })
 
         app.post("/order", async (req, res) => {
